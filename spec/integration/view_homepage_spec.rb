a4761e7c730cdbc71a53ae0a45739865d947f0b6
dsilver829 <dsilver829@aol.com>
2013-09-24 09:28:14 -0700

    Ensure viewing the homepage works

--- spec/integration/view_homepage_spec.rb
+++ spec/integration/view_homepage_spec.rb
@@ -4,5 +4,6 @@ feature 'View the homepage' do
   scenario 'user sees relevant information' do
     visit root_path
     expect(page).to have_title 'Todos'
+    expect(page).to have_css '[data-role="description"]'
   end
 end
