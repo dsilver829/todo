8becb48b1fc679274a74de2e146bd0a588f1f74e
dsilver829 <dsilver829@aol.com>
2013-09-25 05:59:31 -0700

    refactor to create current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def sign_in_as(email)
+    session[:current_email] = email
+  end
+
+  def current_user
+    OpenStruct.new(email: session[:current_email])
+  end
+  helper_method :current_user
 end
