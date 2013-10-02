70c817c645fd87632187b77df33bd39e89db4d20
dsilver829 <dsilver829@aol.com>
2013-10-02 16:22:11 -0700

    refactor specs

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -2,13 +2,17 @@ require 'spec_helper'
 
 feature "Feature: Manage todos" do
   scenario "create a new todo" do
-    visit root_path
-    fill_in 'Email address', with: 'person@example.com'
-    click_button 'Sign In'
+    sign_in
     click_link 'Add a new todo'
     fill_in 'Description', with: 'Buy some milk'
     click_button 'Create todo'
 
     expect(page).to have_css('li.todo'), text: 'Buy some milk'
+  end
+
+  def sign_in
+    visit root_path
+    fill_in 'Email address', with: 'person@example.com'
+    click_button 'Sign In'
   end
 end
