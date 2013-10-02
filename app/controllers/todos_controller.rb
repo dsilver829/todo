14db6c8590a7f2e8f578a2ef71374234b0018d67
dsilver829 <dsilver829@aol.com>
2013-10-02 16:20:30 -0700

    create Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,15 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
   end
 
   def new
+    @todo = Todo.new
+  end
+
+  def create
+    @todo = Todo.new(params[:todo])
+    @todo.save
+    redirect_to todos_url
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Todos::Application.routes.draw do
   root to: 'high_voltage/pages#show', id: 'homepage'
 
   resource  :session, only: [:create]
-  resources :todos, only: [:index, :new]
+  resources :todos, only: [:index, :new, :create]
 end
