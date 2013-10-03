1d1a2571715162c25d7a0d9d4d68118d566bbb79
dsilver829 <dsilver829@aol.com>
2013-10-03 10:58:50 -0700

    create sign_in_as helper

--- spec/support/sign_in_helpers.rb
+++ spec/support/sign_in_helpers.rb
@@ -1,7 +1,11 @@
 module SignInHelpers
   def sign_in
+    sign_in_as 'person@example.com'
+  end
+
+  def sign_in_as(email)
     visit root_path
-    fill_in 'Email address', with: 'person@example.com'
+    fill_in 'Email address', with: email
     click_button 'Sign In'
   end
 end
