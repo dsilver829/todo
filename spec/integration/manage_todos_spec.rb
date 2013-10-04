714a167c4492fa6c1cd0bbe0ca4bb1e42d2b8217
dsilver829 <dsilver829@aol.com>
2013-10-04 11:51:36 -0700

    fix spec bu

--- spec/integration/manage_todos_spec.rb
+++ spec/integration/manage_todos_spec.rb
@@ -3,10 +3,22 @@ require 'spec_helper'
 feature "Feature: Manage todos" do
   scenario "create a new todo" do
     sign_in
+
     click_link 'Add a new todo'
     fill_in 'Description', with: 'Buy some milk'
     click_button 'Create todo'
 
-    expect(page).to have_css('li.todo'), text: 'Buy some milk'
+    expect(page).to have_css('li.todo', text: 'Buy some milk')
+  end
+
+  scenario 'view only my todos' do
+    Todo.create(description: 'Buy some eggs')
+    sign_in
+
+    click_link 'Add a new todo'
+    fill_in 'Description', with: 'Buy some milk'
+    click_button 'Create todo'
+
+    expect(page).to have_css('li.todo', text: 'Buy some milk')
   end
 end
